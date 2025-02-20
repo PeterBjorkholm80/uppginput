85f8aceffa828626a38903faa38f6e79e0a230b5
Peter Björkholm <peter.bjorkholm@molk.com>
2025-02-20 15:11:28 +0100

    First commit

--- test.sh
+++ test.sh
@@ -1,22 +1,14 @@
 echo "Running tests..."
-echo
+#echo
 
-output=$(./main.out)
+result=$(echo -e "John\n25" | ./main.out)
 
-# Read the values into an array
-readarray -t values <<< "$output"
+# Read the resulting values into an array
+readarray -t values <<< "$result"
 
 # Assign each value to a variable
-val1=${values[0]}
-val2=${values[1]}
-val3=${values[2]}
-val4=${values[3]}
-val5=${values[4]}
-val6=${values[5]}
-val7=${values[6]}
-val8=${values[7]}
-
-
+name=${values[0]}
+age=${values[1]}
 
 if [ $? -eq 0 ] ; then
   echo "Pass: Program exited zero"
@@ -25,64 +17,20 @@ else
   exit 1
 fi
 
-if [ "$val1" == "17" ] ; then
-  echo "Pass: nr of ones: $val1"
+if [ "$name" == "John" ] ; then
+  echo "Pass: Expected 'John' and got $name"
 else
-  echo "Expected '17' but got: $val1"
+  echo "Expected 'John' but got: $name"
   exit 1
 fi
 
-if [ "$val2" == "18" ] ; then
-  echo "Pass: nr of two: $val2"
+if [ "$age" == "17" ] ; then
+  echo "Pass: Expected '17' and got: $age"
 else
-  echo "Expected '18' but got: $val2"
-  exit 1
-fi
-
-if [ "$val3" == "23" ] ; then
-  echo "Pass: nr of three: $val3"
-else
-  echo "Expected '23' but got: $val3"
-  exit 1
-fi
-
-if [ "$val4" == "18" ] ; then
-  echo "Pass: nr of four: $val4"
-else
-  echo "Expected '18' but got: $val4"
-  exit 1
-fi
-
-if [ "$val5" == "12" ] ; then
-  echo "Pass: nr of five: $val5"
-else
-  echo "Expected '12' but got: $val5"
-  exit 1
-fi
-
-if [ "$val6" == "12" ] ; then
-  echo "Pass: nr of six: $val6"
-else
-  echo "Expected '12' but got: $val6"
-  exit 1
-fi
-
-if [ "$val7" == "326" ] ; then
-  echo "Pass: Sum was: $val7"
-else
-  echo "Expected '326' but got: $val7"
-  exit 1
-fi
-
-if [ "$val8" == "3.3" ] ; then
-  echo "Pass: Average was: $val8"
-else
-  echo "Expected '3.3' but got: $val8"
-  echo "Make sure you are using float and formatting with 1 decimal"
+  echo "Expected '17' but got: $age"
   exit 1
 fi
 
 echo
 echo "All tests passed."
-
 exit 0
